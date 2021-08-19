b14f58bb64819f7984b2ba3c62b1653f6d9d0f13
cn8817 <christinenguyen1433@gmail.com>
2021-08-18 19:29:21 -0700

    completed the project

--- client/src/components/EditMovieForm.js
+++ client/src/components/EditMovieForm.js
@@ -6,6 +6,17 @@ import axios from 'axios';
 
 const EditMovieForm = (props) => {
 	const { push } = useHistory();
+	const { id } = useParams()
+
+	useEffect(()=> {
+		axios.get(`http://localhost:5000/api/movies/${id}`)
+			.then(res=> {
+				setMovie(res.data)
+			})
+			.catch(err=>{
+				console.log(err)
+			})
+	}, [])
 
 	const [movie, setMovie] = useState({
 		title:"",
@@ -24,6 +35,14 @@ const EditMovieForm = (props) => {
 
     const handleSubmit = (e) => {
 		e.preventDefault();
+		axios.put(`http://localhost:5000/api/movies/${id}`, movie)
+			.then(res=> {
+				push(`/movies/${id}`)
+				props.setMovie(res.data)
+			})
+			.catch(err=> {
+				console.log(err)
+			})
 	}
 	
 	const { title, director, genre, metascore, description } = movie;
